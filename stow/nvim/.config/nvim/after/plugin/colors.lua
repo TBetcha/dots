6a26b6cae66c6ad21387c73604fc02eac55965a2
TMB <troy.boettger@gmail.com>
2023-10-06 00:59:33 -0400

    colors

--- stow/nvim/.config/nvim/after/plugin/colors.lua
+++ stow/nvim/.config/nvim/after/plugin/colors.lua
@@ -8,7 +8,7 @@ end
 
 function LineNumberColors()
     vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='gray', bold=false })
-    vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg='#a6e3a1', bold=true })
     vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='gray', bold=false })
 end
 
