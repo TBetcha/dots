28a7590e7c5119bed70737b9008b92f5195e45e0
Tbetcha <troy.boettger@gmail.com>
2024-01-24 10:47:16 -0500

    stuff

--- stow/sketchybar/.config/sketchybar/items/window_title.sh
+++ stow/sketchybar/.config/sketchybar/items/window_title.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 sketchybar --add item space_separator left                            \
---add item window_title center                               \
+--add item window_title left                               \
 --set window_title    script="$PLUGIN_DIR/window_title.sh" \
 icon.drawing=off                     \
 label.font="$FONT:Semibold:15.0"         \
